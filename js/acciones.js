02e4b134dd54e6e14547ba5a4f3d7bdb253b9157
Maferoye <vio_tamagochi@hotmail.com>
2014-12-17 11:49:11 -0600

    15

--- js/acciones.js
+++ js/acciones.js
@@ -49,7 +49,7 @@ function Listado(Quien)
 			for (var i=0; i<DatosJSON.peli.length;i++)
 			{
 				
-			  $('#Tabla').append('<div style="float:left; width:50%"><h4>Id Pelicula:'+DatosJSON.peli[i].Id+'</h4><div id="RIdPelicula"></div><h4>Nombre:'+DatosJSON.peli[i].Nombre+'</h4><div id="RNombre"></div><h4>Director:'+DatosJSON.peli[i].Director+'</h4><div id="RDirector"></div><h4>Genero:'+DatosJSON.peli[i].Genero+'</h4><div id="RGenero"></div><h4>Duracion:'+DatosJSON.peli[i].Duracion+'</h4><div id="RDuracion"></div><div style="float:right; width:50%"><img src="http://192.168.1.177/catalogopeliculas/recursos/fotos/'+DatosJSON.peli[i].Id+'.jpg" class="FotosP"></div></div><strong><hr></strong>');	
+			  $('#Tabla').append('<div align="justify" style="clear:both"><div style="float:left; width:50%"><h4>Id Pelicula:</h4><div id="RIdPelicula">'+DatosJSON.peli[i].Id+'</div><h4>Nombre:</h4><div id="RNombre">'+DatosJSON.peli[i].Nombre+'</div><h4>Director:</h4><div id="RDirector">'+DatosJSON.peli[i].Director+'</div><h4>Genero:</h4><div id="RGenero">'+DatosJSON.peli[i].Genero+'</div><h4>Duracion:</h4><div id="RDuracion">'+DatosJSON.peli[i].Duracion+'minutos</div></div><div style="float:left; width:50%"><img style="width:100%" src="http://192.168.1.177/catalogopeliculas/recursos/fotos/'+DatosJSON.peli[i].Id+'.jpg" class="FotosP"></div><strong><hr></strong></div>');	
 			}
 			$('.FotosP').width($('#Tabla').width()*.2);
 			$('#Nom').trigger('pagecreate');
